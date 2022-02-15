8063e64b535960cf9001434998745b17d644b3a8
Jónas Orri Daníelsson <43910628+xJod@users.noreply.github.com>
2022-02-15 14:36:37 +0000

    Update 2020_N_Fyrirtaekjasafn.py

--- Delta_2020/2020_N_Fyrirtaekjasafn.py
+++ Delta_2020/2020_N_Fyrirtaekjasafn.py
@@ -2,11 +2,11 @@
 
 s = str(input())															# Tek inn streng
 
-serhlodar = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y']	# Listi af öllum sérhljóðum
+serhljodar = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y']	# Listi af öllum sérhljóðum
 
 finalS = []																	# Tómur listi
 for i in range(len(s)):														# Loopa lengdina af s
-	if s[i] in serhlodar:													# Ef stafurinn er í sérhljóða listanum
+	if s[i] in serhljodar:													# Ef stafurinn er í sérhljóða listanum
 		finalS.append(s[i])													# Set hann í nýja listann
 
 print(''.join(finalS))														# Prenta loka listann sem streng
